e5c4f912c8ebf04f7d1d1a9538bb32081275523c
darkfranko <fsnq2@mail.missouri.edu>
2021-12-10 15:36:51 -0600

    Working project. Need to fix admin login - Francekso

--- it-4320-final-proj/flask_wtforms_tutorial/routes.py
+++ it-4320-final-proj/flask_wtforms_tutorial/routes.py
@@ -21,7 +21,7 @@ def user_options():
 
 @app.route("/admin", methods=['GET', 'POST'])
 def admin():
-
+    error = None
     form = AdminLoginForm()
 
     return render_template("admin.html", form=form, template="form-template")
@@ -32,4 +32,15 @@ def reservations():
     form = ReservationForm()
 
     return render_template("reservations.html", form=form, template="form-template")
+# Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    admin = "admin1"
+    if request.method == 'POST':
+        if request.form['username'] != admin  or request.form['password'] != '12345':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
 
